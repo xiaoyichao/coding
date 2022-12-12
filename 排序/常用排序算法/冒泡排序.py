6bda0a4a9620441b6ab577ac1489cad35b00cfe5
root <root@haohaozhu.com>
2022-12-12 13:26:59 +0800

    自己的才是自己的，这是病愈后第一次写代码

--- 排序/常用排序算法/冒泡排序.py
+++ 排序/常用排序算法/冒泡排序.py
@@ -3,7 +3,7 @@
 Author: 
 LastEditors: xiaoyichao
 Date: 2022-05-17 08:43:54
-LastEditTime: 2022-12-04 23:17:35
+LastEditTime: 2022-12-06 14:28:17
 Description: 
 
 每一趟选出一个最大值，排在最后一个
@@ -13,7 +13,7 @@ https://leetcode.cn/problems/kth-largest-element-in-an-array/solution/ge-chong-p
 
 '''
 
-
+# 基础冒泡
 def bubble_sort(alist):
     alist_len = len(alist)
     for i in range(1, alist_len):
@@ -23,20 +23,7 @@ def bubble_sort(alist):
 
     return alist
 
-
-def bubble_sort(alist):
-    n = len(alist)
-    for i in range(n-1, 0, -1):
-        count = 0
-        for j in range(i):
-            if alist[j] > alist[j+1]:
-                alist[j], alist[j+1] = alist[j+1], alist[j]
-                count += 1
-        if count == 0:
-            break
-    return alist
-
-
+# 增加了early stop的冒泡
 def bubble_sort(alist):
     n = len(alist)
     for i in range(n-1, 0, -1):
@@ -49,6 +36,16 @@ def bubble_sort(alist):
             break
     return alist
 
+def bubble_sort(alist):
+    n = len(alist)
+    for i in range(n-1, 0, -1):
+        count = 0
+        for j in range(0,i,1):
+            if alist[j]>alist[j+1]:
+                alist[j], alist[j+1] = alist[j+1], alist[j]
+        if count >0:
+            break
+    return alist
 
 alist = bubble_sort([1, 5, 3, 6, 3, 9, 8])
 print(alist)
