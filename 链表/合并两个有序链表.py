756728c664fb2cd7156ff1fee4c59ddc9b913ae1
肖轶超 <30403377+xiaoyichao@users.noreply.github.com>
2022-12-20 20:41:28 +0800

    链表

--- 链表/合并两个有序链表.py
+++ 链表/合并两个有序链表.py
@@ -3,7 +3,7 @@
 Author: xiaoyichao
 LastEditors: xiaoyichao
 Date: 2022-12-15 17:51:42
-LastEditTime: 2022-12-16 15:48:59
+LastEditTime: 2022-12-20 20:35:26
 Description: 
 https://leetcode.cn/problems/merge-two-sorted-lists/
 
@@ -34,21 +34,22 @@ class Solution:
 
 
 class Solution:
-    # 迭代
-    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        prehead = ListNode(-1)
-
+    # 迭代，本质是使用了双指针+单指针
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        prehead = ListNode(-1) #将头节点的val置为-1，当然也可以置为其他数 
         prev = prehead
-        while l1 and l2:
-            if l1.val <= l2.val:
-                prev.next = l1
-                l1 = l1.next
+        while list1 and list2:
+            if list1.val < list2.val:
+                prev.next = list1 # l1的指针下移
+                list1 = list1.next
             else:
-                prev.next = l2
-                l2 = l2.next            
-            prev = prev.next
-
+                prev.next = list2 # l2的指针下移 
+                list2 = list2.next
+            prev = prev.next # 单指针下移 
         # 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
-        prev.next = l1 if l1 is not None else l2
+        if list1:
+            prev.next = list1 
+        else:
+            prev.next = list2
+        return prehead.next # 不要头节点
 
-        return prehead.next
