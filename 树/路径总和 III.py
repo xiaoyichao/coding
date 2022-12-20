857572911e3d836c5ab1888bee209503d2c49568
肖轶超 <30403377+xiaoyichao@users.noreply.github.com>
2022-12-20 10:53:44 +0800

    test

--- 树/路径总和 III.py
+++ 树/路径总和 III.py
@@ -3,8 +3,8 @@
 Author: xiaoyichao
 LastEditors: xiaoyichao
 Date: 2022-12-19 15:42:04
-LastEditTime: 2022-12-19 15:44:05
-Description: 
+LastEditTime: 2022-12-19 17:54:50
+Description: https://leetcode.cn/problems/path-sum-iii/solution/lu-jing-zong-he-iii-by-leetcode-solution-z9td/
 '''
 from typing import Optional
 
@@ -17,36 +17,22 @@ class TreeNode:
         self.right = right
 
 
-class Solution: #路径总和3
-    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
-        if not root:
+class Solution: #路径总和3 
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int: # 深度优先搜索
+        def rootsum(root, targetSum):
+            if root is None:
+                return 0
+            ret = 0
+            if root.val == targetSum:
+                ret += 1
+            ret += rootsum(root.left, targetSum - root.val)
+            ret += rootsum(root.right, targetSum - root.val)
+            return ret
+
+        if root is None:    
             return 0
-
-        res = 0
-                
-        def dfs(root,targetSum,path):
-            if not root:        
-
-                return     
-            path.append(root.val)   
-
-            if not root.left and not root.right and sum(path) == targetSum:
-
-                res += 1
-                    
-            dfs(root.left,targetSum,path)
-
+        ret = rootsum(root, targetSum)
+        ret += self.pathSum(root.left, targetSum)
+        ret += self.pathSum(root.right, targetSum)
+        return ret
             
-            dfs(root.right,targetSum,path)
-
-            path.pop()
-
-        dfs(root,targetSum,[])
-        
-
-        return res
-
-
-
-            
-
