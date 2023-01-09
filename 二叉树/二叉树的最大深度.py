75300f4b8ecb8865d403b626296dda8eda7fcaf8
root <root@haohaozhu.com>
2023-01-09 11:18:35 +0800

    好好住真的不好

--- 二叉树/二叉树的最大深度.py
+++ 二叉树/二叉树的最大深度.py
@@ -3,20 +3,49 @@
 Author: xiaoyichao
 LastEditors: xiaoyichao
 Date: 2022-12-16 17:09:42
-LastEditTime: 2022-12-16 17:48:59
+LastEditTime: 2023-01-06 18:56:44
 Description: 
 
 https://leetcode.cn/problems/maximum-depth-of-binary-tree/solution/er-cha-shu-de-zui-da-shen-du-by-leetcode-solution/
 
 '''
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
 # 深度优先搜索
 class Solution:
-    def maxDepth(self, root):
-        if root is None: 
-            return 0 
-        else: 
-            left_height = self.maxDepth(root.left) 
-            right_height = self.maxDepth(root.right) 
-            return max(left_height, right_height) + 1 
+    # def maxDepth(self, root):
+    #     if root is None: 
+    #         return 0 
+    #     else: 
+    #         left_height = self.maxDepth(root.left) 
+    #         right_height = self.maxDepth(root.right) 
+    #         return max(left_height, right_height) + 1 
 
+
+    def maxDepth(self, root):
+        res = 0
+        depth  = 0 
+        self.traverse(root)
+        return res
+
+    def traverse(self, root):
+        if root is None:
+            return
+        depth +=1
+        if root.left is None and root.right is None:
+            res = max(res,depth)
+        self.traverse(root.left)
+        self.traverse(root.right)
+        depth -=1
+            
+
+def maxDepth(root):
+    if root is None:
+        return 0
+    return max(maxDepth(root.left), maxDepth(root.right)) + 1
