483ccfa57f01e3f3dc2376e3c0f3ec96a7b45885
肖轶超 <30403377+xiaoyichao@users.noreply.github.com>
2023-01-13 13:49:26 +0800

    test

--- 回溯算法/子集.py
+++ 回溯算法/子集.py
@@ -3,15 +3,53 @@
 Author: xiaoyichao
 LastEditors: xiaoyichao
 Date: 2023-01-12 18:46:43
-LastEditTime: 2023-01-12 18:50:09
+LastEditTime: 2023-01-13 13:15:40
 Description: https://leetcode.cn/problems/subsets/
 '''
 from typing import List
+
+# labuladong 的写法
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
         track = []
-        self.backtrack(nums, 0, res)
+
+        def backtrack(nums, start):
+            # 遍历到了第 k 层，收集当前节点的值
+            # 把当前节点放到 track 中，
+            # 然后递归到下一层
+            res.append(track[:])
+            for i in range(start, len(nums)):
+                track.append(nums[i])
+                backtrack(nums, i + 1)
+                track.pop()
+
+        backtrack(nums, 0)
         return res
-    def backtrack(self, nums, start, res):
-        res.
+
+
+
+s = Solution()
+s.subsets([1,2,3])
+
+# 其他人的写法
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        track = []
+        n = len(nums)
+        def backtrack(i: int) -> None:
+            res.append(track.copy())  # 固定答案
+            if i == n:
+                return
+            for j in range(i, n):  # 枚举选择的数字
+                track.append(nums[j])
+                backtrack(j + 1)
+                track.pop()  # 恢复现场
+        backtrack(0)
+        return res
+
+
+
+s = Solution()
+s.subsets([1,2,3])
