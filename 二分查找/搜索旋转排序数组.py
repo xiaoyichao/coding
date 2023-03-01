5ac1569f4f4eb07865699556275f252c01b43e1f
xiaoyichao <xiao_yi_chao@163.com>
2023-03-01 17:16:50 +0800

    周三第一道

--- 二分查找/搜索旋转排序数组.py
+++ 二分查找/搜索旋转排序数组.py
@@ -6,3 +6,19 @@ from typing import List
 
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:  # 二分查找循环条件
+            mid = (left + right) // 2  # 找到中间位置
+            if nums[mid] == target:  # 如果中间位置的值等于目标值，直接返回该位置
+                return mid
+            if nums[mid] >= nums[left]:  # 如果中间位置的值大于等于最左边的值，说明左半部分数组有序
+                if nums[left] <= target < nums[mid]:  # 如果目标值在左半部分数组中，缩小查找范围到左半部分数组
+                    right = mid - 1
+                else:  # 如果目标值不在左半部分数组中，缩小查找范围到右半部分数组
+                    left = mid + 1
+            else:  # 如果中间位置的值小于最左边的值，说明右半部分数组有序
+                if nums[mid] < target <= nums[right]:  # 如果目标值在右半部分数组中，缩小查找范围到右半部分数组
+                    left = mid + 1
+                else:  # 如果目标值不在右半部分数组中，缩小查找范围到左半部分数组
+                    right = mid - 1
+        return -1  # 如果找不到目标值，返回 -1
