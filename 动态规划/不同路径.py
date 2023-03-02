d3b0a91520224166b885b109570c1727c75189ec
xiaoyichao <xiao_yi_chao@163.com>
2023-03-02 14:18:43 +0800

    周四-3

---
+++ 动态规划/不同路径.py
@@ -0,0 +1,10 @@
+"https://leetcode.cn/problems/unique-paths/"
+
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [ [1]*n for _ in range(m)]
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] +dp[i][j-1]
+        # return dp[m-1][n-1] 
+        return dp[-1][-1] 
