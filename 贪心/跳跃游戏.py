d1868f851e572a30cf30604457a0e2a45388d6a2
xiaoyichao <xiao_yi_chao@163.com>
2023-03-02 11:51:33 +0800

    周四-1

---
+++ 贪心/跳跃游戏.py
@@ -0,0 +1,31 @@
+"https://leetcode.cn/problems/jump-game/"
+
+from typing import List
+
+# 这段代码更优美
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        max_reach = 0
+        for i in range(n):
+            if i > max_reach:
+                return False
+            max_reach = max(max_reach, i + nums[i])
+        return max_reach >= n - 1
+
+
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        n = len(nums)
+        if n == 0:
+            return False
+        furthest = nums[0]
+        for i in range(1, n):
+            if furthest >= i:
+                furthest = max(furthest, nums[i] + i)
+            else:
+                return False
+        return furthest >= n - 1
+
+
+
