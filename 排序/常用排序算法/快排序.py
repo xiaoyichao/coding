968126f4ec531f96ae76267300a6ecf2dd36444b
xiaoyichao <xiao_yi_chao@163.com>
2023-03-16 18:06:17 +0800

    none

--- 排序/常用排序算法/快排序.py
+++ 排序/常用排序算法/快排序.py
@@ -41,89 +41,36 @@ class Solution: # 建议用这个
             return self.sortArray(left) + mid + self.sortArray(right)
 
 
+import random
+class Solution:  #快排序
+    def sortArray(self, nums: List[int]) -> List[int]:
+        def quick_sort(nums, start, end):
+            if start >= end:
+                return
 
-def quick_sort(nums):
-    if len(nums)<=1:
+            # 将随机主元移动到最后一个位置
+            povit_index = random.randint(start, end)
+            nums[povit_index], nums[end] = nums[end], nums[povit_index]
+
+            povit = nums[end]
+            i,j = start, end-1
+            while i<=j:
+                while i<=j and nums[j]>povit:
+                    j-=1
+                while i<=j and nums[i]<povit:
+                    i+=1
+                if i<=j:
+                    nums[i], nums[j] = nums[j], nums[i]
+                    i+=1
+                    j-=1
+            nums[i], nums[end] = nums[end], nums[i]
+            quick_sort(nums, start, i-1)
+            quick_sort(nums, i+1, end)
+        
+        quick_sort(nums, 0, len(nums)-1)
         return nums
-    else:
-        pivot = nums[-1]
-        left = []
-        right = []
-        for num in nums[:-1]:
-            if num <= pivot:
-                left.append(num)
-            else:
-                right.append(num)
-        return quick_sort(left) + [pivot] + quick_sort(right)
-
-def quick_sort(alist, start, end):
-    """快速排序"""
-    if start >= end:  # 递归的退出条件
-        return
-    mid = alist[start]  # 设定起始的基准元素
-    low = start  # low为序列左边在开始位置的由左向右移动的游标
-    high = end  # high为序列右边末尾位置的由右向左移动的游标
-    while low < high:
-        # 如果low与high未重合，high(右边)指向的元素大于等于基准元素，则high向左移动
-        while low < high and alist[high] >= mid:
-            high -= 1
-        alist[low] = alist[high]  # 走到此位置时high指向一个比基准元素小的元素,将high指向的元素放到low的位置上,此时high指向的位置空着,接下来移动low找到符合条件的元素放在此处
-        # 如果low与high未重合，low指向的元素比基准元素小，则low向右移动
-        while low < high and alist[low] <= mid:
-            low += 1
-        alist[high] = alist[low]  # 此时low指向一个比基准元素大的元素,将low指向的元素放到high空着的位置上,此时low指向的位置空着,之后进行下一次循环,将high找到符合条件的元素填到此处
-
-    # 退出循环后，low与high重合，此时所指位置为基准元素的正确位置,左边的元素都比基准元素小,右边的元素都比基准元素大
-    alist[low] = mid  # 将基准元素放到该位置,
-    # 对基准元素左边的子序列进行快速排序
-    quick_sort(alist, start, low - 1)  # start :0  low -1 原基准元素靠左边一位
-    # 对基准元素右边的子序列进行快速排序
-    quick_sort(alist, low + 1, end)  # low+1 : 原基准元素靠右一位  end: 最后
 
 
-
-def quick_sort_1(alist, start, end):
-    if start >= end:
-        return
-    middle = start
-    low = start
-    high = end
-    while low < high:
-        while low < high and alist[high] >= alist[middle]:
-            high -= 1
-        alist[low] = alist[high]
-        while low < high and alist[low] <= alist[middle]:
-            low += 1
-        alist[high] = alist[low]
-    alist[low] = alist[middle]
-    quick_sort_1(alist, start, low-1)
-    quick_sort_1(alist, low+1, end)
-
-
-def quick_sort_2(alist, start, end):
-    if start >= end:
-        return
-    middle = start
-    low = start
-    high = end
-    while low < high:
-        while low < high and alist[high] >= alist[middle]:
-            high -= 1
-        alist[low] = alist[high]
-        while low < high and alist[low] <= alist[middle]:
-            low += 1
-        alist[high] = alist[low]
-    alist[low] = alist[middle]
-    quick_sort_2(alist, low+1, end)
-    quick_sort_2(alist, start, low-1)
-
-
-if __name__ == '__main__':
-    alist = [54, 54, 93, 66, 66, 31, 44, 55, 55]
-    quick_sort(alist, 0, len(alist) - 1)
-    print(alist)
-    alist = [54, 54, 93, 66, 66, 31, 44, 55, 55]
-    quick_sort_1(alist, 0, len(alist)-1)
-    print(alist)
-
-
+s = Solution()
+res = s.sortArray([5,2,3,1])
+print(res)
