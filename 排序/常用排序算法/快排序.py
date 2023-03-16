71e2c2914ff1ca4eddc7c5f4a86ef931716deb43
xiaoyichao <xiao_yi_chao@163.com>
2023-03-16 19:11:26 +0800

    4-2

--- 排序/常用排序算法/快排序.py
+++ 排序/常用排序算法/快排序.py
@@ -41,33 +41,47 @@ class Solution: # 建议用这个
             return self.sortArray(left) + mid + self.sortArray(right)
 
 
+
 import random
+
 class Solution:  #快排序
     def sortArray(self, nums: List[int]) -> List[int]:
         def quick_sort(nums, start, end):
+            # 如果 start 大于等于 end，说明数组已经排好序，返回
             if start >= end:
                 return
-
-            # 将随机主元移动到最后一个位置
+            
+            # 随机生成枢纽元素的下标
             povit_index = random.randint(start, end)
+            
+            # 将枢纽元素和数组末尾元素交换位置
             nums[povit_index], nums[end] = nums[end], nums[povit_index]
-
+            # 将枢纽元素设为数组末尾元素的值
             povit = nums[end]
-            i,j = start, end-1
-            while i<=j:
-                while i<=j and nums[j]>povit:
-                    j-=1
-                while i<=j and nums[i]<povit:
-                    i+=1
-                if i<=j:
+
+            # 定义两个指针 i 和 j，分别指向数组开头和结尾的元素
+            i, j = start, end - 1
+            # 当 i 小于等于 j 时进行循环
+            while i <= j:
+                # 在 i 小于等于 j 且 nums[i] 小于枢纽元素时，i 向右移动
+                while i <= j and nums[i] < povit:
+                    i += 1
+                # 在 i 小于等于 j 且 nums[j] 大于枢纽元素时，j 向左移动
+                while i <= j and nums[j] > povit:
+                    j -= 1
+                # 如果 i 小于等于 j，则将 nums[i] 和 nums[j] 交换位置
+                if i <= j:
                     nums[i], nums[j] = nums[j], nums[i]
-                    i+=1
-                    j-=1
+                    i += 1
+                    j -= 1
+                    
+            # 最后将枢纽元素放到正确的位置，将数组分为两半分别递归快排
             nums[i], nums[end] = nums[end], nums[i]
-            quick_sort(nums, start, i-1)
-            quick_sort(nums, i+1, end)
+            quick_sort(nums, start, i - 1)
+            quick_sort(nums, i + 1, end)
         
-        quick_sort(nums, 0, len(nums)-1)
+        # 调用快速排序函数，对 nums 进行排序，起始下标为 0，结束下标为数组长度 - 1
+        quick_sort(nums, 0, len(nums) - 1)
         return nums
 
 
