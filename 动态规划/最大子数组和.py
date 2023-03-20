1cec656bd6eac6bb1fc36c85bf57b80ef0440971
xiaoyichao <xiao_yi_chao@163.com>
2023-03-20 18:49:14 +0800

    1-3

--- 动态规划/最大子数组和.py
+++ 动态规划/最大子数组和.py
@@ -5,12 +5,8 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        
-        res = 0
         dp = [nums[0]]
-        n = len(nums)-1
-        for i in range(1, n+1):
+        for i in range(1, len(nums)):
             dp.append(max(nums[i], dp[i-1]+nums[i]))
-
         res = max(dp)
         return res
