cd5fa3835d784ae97d0058cf38384802f9bf1dfb
xiaoyichao <xiao_yi_chao@163.com>
2023-03-20 22:05:01 +0800

    1-4

--- NSUM 问题/三数之和.py
+++ NSUM 问题/三数之和.py
@@ -20,25 +20,25 @@ class Solution:
             _sum = nums[lo] + nums[hi]
             left, right = nums[lo], nums[hi]
             if _sum < target:
-                while lo < hi and nums[lo] == nums[lo + 1]: # 跳过相同的元素
-                    lo += 1
-                lo += 1  # 本身的迭代
+                # while lo < hi and nums[lo] == nums[lo + 1]: # 跳过相同的元素
+                #     lo += 1
+                # lo += 1  # 本身的迭代
 
                 # 这种写法也可以，但是有点晦涩
-                # while lo < hi and nums[lo] == left:
-                #     lo += 1
+                while lo < hi and nums[lo] == left:
+                    lo += 1
             elif _sum > target:
-                while lo < hi and nums[hi] == nums[hi - 1]:
+                while lo < hi and nums[hi] == right:
                     hi -= 1
-                hi -= 1
+                # hi -= 1
             else: # _sum == target
                 res.append([nums[lo], nums[hi]])
-                while lo < hi and nums[lo] == nums[lo + 1]: # 跳过相同的元素
+                while lo < hi and nums[lo] == left: # 跳过相同的元素
                     lo += 1
-                lo += 1 # 本身的迭代
-                while lo < hi and nums[hi] == nums[hi - 1]:
+                # lo += 1 # 本身的迭代
+                while lo < hi and nums[hi] == right:
                     hi -= 1
-                hi -= 1
+                # hi -= 1
         return res
 
 
@@ -63,29 +63,70 @@ class Solution:
         return res
 
     # 其他的方法
-    # def threeSum(self, nums: List[int]) -> List[List[int]]:
-    #     nums.sort()  
-    #     res = []
-    #     for k in range(0, len(nums) -1):
-    #         i = k+1
-    #         j = len(nums) -1
-    #         if(k>0 and nums[k]==nums[k-1]): # k循环的这一层去重复
-    #             continue 
-    #         while i<j:
-    #             if nums[i] + nums[j] + nums[k] == 0  and i!=j and j!=k and i!=k:  
-    #                 res.append([nums[i], nums[j], nums[k]])
-    #                 while nums[i] == nums[i+1] and i< len(nums)-2:  # i循环的这一层去重复
-    #                     i+=1
-    #                 while nums[j] == nums[j-1] and j>0: # j循环的这一层去重复
-    #                     j-=1
-    #                 i+=1
-    #                 j-=1
-    #             elif nums[i] + nums[j] + nums[k]> 0 :
-    #                 j-=1
-    #             else:
-    #                 i+=1
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()  
+        res = []
+        for k in range(0, len(nums) -1):
+            i = k+1
+            j = len(nums) -1
+            if(k>0 and nums[k]==nums[k-1]): # k循环的这一层去重复
+                continue 
+            while i<j:
+                if nums[i] + nums[j] + nums[k] == 0  and i!=j and j!=k and i!=k:  
+                    res.append([nums[i], nums[j], nums[k]])
+                    while nums[i] == nums[i+1] and i< len(nums)-2:  # i循环的这一层去重复
+                        i+=1
+                    while nums[j] == nums[j-1] and j>0: # j循环的这一层去重复
+                        j-=1
+                    i+=1
+                    j-=1
+                elif nums[i] + nums[j] + nums[k]> 0 :
+                    j-=1
+                else:
+                    i+=1
 
-    #     return res
+        return res
+    
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # 对原始数组进行排序
+        nums.sort()
+        n = len(nums)
+        res = []
+        
+        # 枚举第一个数
+        for i in range(n):
+            # 如果第一个数大于0，后面的数一定大于0，直接结束循环
+            if nums[i] > 0:
+                break
+            # 如果第一个数和前一个数相同，跳过本次循环，避免重复
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            
+            # 双指针查找第二个数和第三个数
+            left, right = i + 1, n - 1
+            while left < right:
+                # 如果三数之和等于0，将这三个数添加到结果列表中
+                if nums[i] + nums[left] + nums[right] == 0:
+                    res.append([nums[i], nums[left], nums[right]])
+                    # 跳过重复的第二个数
+                    while left < right and nums[left] == nums[left+1]:
+                        left += 1
+                    # 跳过重复的第三个数
+                    while left < right and nums[right] == nums[right-1]:
+                        right -= 1
+                    left += 1
+                    right -= 1
+                # 如果三数之和小于0，将左指针右移
+                elif nums[i] + nums[left] + nums[right] < 0:
+                    left += 1
+                # 如果三数之和大于0，将右指针左移
+                else:
+                    right -= 1
+        
+        return res
+
 
 s = Solution()
 nums = [-1,0,1,2,-1,-4]
