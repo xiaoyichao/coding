177e8e7b9481d42ea2b09c61c5067f8100deba32
xiaoyichao <xiao_yi_chao@163.com>
2023-03-20 22:48:44 +0800

    1-6

--- 二叉树和N叉树/二叉树的层序遍历.py
+++ 二叉树和N叉树/二叉树的层序遍历.py
@@ -15,20 +15,36 @@ class TreeNode:
         self.left = left
         self.right = right
 
+# 可以使用广度优先搜索（BFS）来进行二叉树的层序遍历，通过维护一个队列来实现。
+
+# 具体步骤如下：
+
+# 1 将根节点加入队列。
+
+# 2 当队列不为空时，依次取出队列中的节点，并将其左右子节点（如果存在）加入队列。
+
+# 3 将取出节点的值记录到当前层的结果列表中。
+
+# 4 遍历完当前层后，将结果列表加入最终结果列表中。
+
+# 重复步骤2-4，直到队列为空。
 class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        if root is None:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        if not root:
             return []
-        ans = []
-        q = deque([root])
-        while q:
-            vals = []
-            for _ in range(len(q)):
-                node = q.popleft() 
-                vals.append(node.val)
-                if node.left:  q.append(node.left)  #向队列中添加数据
-                if node.right: q.append(node.right) #向队列中添加数据
-            ans.append(vals)
-        return ans
+        res = []
+        queue = [root]
+        while queue:
+            level = []
+            size = len(queue)
+            for i in range(size):
+                node = queue.pop(0)
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(level)
+        return res
 
 
