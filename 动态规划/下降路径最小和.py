cae4bb2af73a584caf35aad8dfaeb96252adea68
xiaoyichao <xiao_yi_chao@163.com>
2023-04-11 10:55:01 +0800

    2-1

--- 动态规划/下降路径最小和.py
+++ 动态规划/下降路径最小和.py
@@ -1,6 +1,32 @@
 "https://leetcode.cn/problems/minimum-falling-path-sum/"
 
 from typing import List
+
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        n = len(matrix)
+        # 初始化dp数组
+        dp = [[float('inf')] * n for _ in range(n)]
+        for i in range(n):
+            dp[0][i] = matrix[0][i]
+        # 动态规划求解
+        for i in range(1, n):
+            for j in range(n):
+                # 边界情况：上一行第一个元素或最后一个元素
+                if j == 0:
+                    dp[i][j] = min(dp[i - 1][j], dp[i - 1][j + 1]) + matrix[i][j]
+                elif j == n - 1:
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j]) + matrix[i][j]
+                else:
+                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i - 1][j + 1]) + matrix[i][j]
+        # 取最小值
+        return min(dp[-1])
+
+
+s= Solution()
+res = s.minFallingPathSum([[2,1,3],[6,5,4],[7,8,9]])
+print(res)
+
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         n = len(matrix)
@@ -25,7 +51,3 @@ class Solution:
                                        mid,
                                        right)
         return mem[i][j]
-
-s= Solution()
-res = s.minFallingPathSum([[2,1,3],[6,5,4],[7,8,9]])
-print(res)
