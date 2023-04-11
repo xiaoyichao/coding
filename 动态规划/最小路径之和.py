7872c1b977d9df181390dc79d93120a979727e45
xiaoyichao <xiao_yi_chao@163.com>
2023-04-11 11:35:47 +0800

    2-4

--- 动态规划/最小路径之和.py
+++ 动态规划/最小路径之和.py
@@ -35,21 +35,6 @@ class Solution:
 
         return dp[-1][-1]
 
-    def minPathSum_1(self, grid: List[List[int]]) -> int:
-        rows = len(grid)
-        columns = len(grid[0])
-        dp = [[0] * rows] * columns
-        dp[0][0] = grid[0][0]
-        for i in range(1, rows):
-            dp[i][0] = dp[i-1][0] + grid[i][0]
-        for j in range(1, columns):
-            dp[0][j] = dp[0][j-1] + grid[0][j]
-        for i in range(1, rows):
-            for j in range(1, columns):
-                dp[i][j] = min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j]
-        return dp[-1][-1]
-
-
 # 空间复杂度可以优化，例如每次只存储上一行的dp 值，则可以将空间复杂度优化到 O(n)
 # class Solution:
 #     def minPathSum(self, grid: List[List[int]]) -> int:
--- 动态规划/最长递增子序列.py
+++ 动态规划/最长递增子序列.py
@@ -3,9 +3,14 @@
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
+        if n == 1:
+            return 1
         dp = [1]*n
+
         for i in range(n):
-            for j in range(0, i,1):
+            for j in range(0, i):
                 if nums[i]> nums[j]:
                     dp[i] = max(dp[i], dp[j]+1)
+
         return max(dp)
+        
