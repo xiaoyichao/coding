9ccbd82d20a8201c08057ca3c3febae51e4b4b53
肖轶超 <30403377+xiaoyichao@users.noreply.github.com>
2023-04-18 16:13:58 +0800

    none

--- 动态规划/不同路径.py
+++ 动态规划/不同路径.py
@@ -5,6 +5,6 @@ class Solution:
         dp = [ [1]*n for _ in range(m)]
         for i in range(1, m):
             for j in range(1, n):
-                dp[i][j] = dp[i-1][j] +dp[i][j-1]
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
         # return dp[m-1][n-1] 
         return dp[-1][-1] 
